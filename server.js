54f97fc66e9775994f82a66d35a856ff3d790555
Prem1kr <prem78334@gmail.com>
2026-05-09 10:23:02 +0530

    send and recived message added

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
+// ===============================
+// BACKEND SOCKET SERVER
+// ===============================
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import ConnectMongoose from "./config/db.js";
 import authRouter from "./routes/authRoute.js";
-import { createServer } from 'node:http';
+import { createServer } from "node:http";
 import { Server } from "socket.io";
 import { socketConnection } from "./config/socket.js";
-// import carRouter from "./routes/carRoute.js";
+// import carRouter from "./routes/carRoute.js"
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
@@ -22,34 +25,47 @@ const io = new Server(server, {
 
 // ROUTES
 app.use('/api/auth', authRouter);
+socketConnection();
 
-// app.use('/api/car', carRouter);
-// socket connection
-socketConnection(io);
-const Room = 'group';
+// STORE ADMIN SOCKET ID
+let adminSocketId = null;
 
 // SOCKET CONNECTION
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
+    console.log('User connected:', socket.id);
 
-    // JOIN ROOM
-    socket.on('joinRoom', (userName) => {
-        socket.join(Room);
-        console.log(`${userName} joined the group with socket id: ${socket.id}`);
-        // SEND TO ALL USERS EXCEPT CURRENT USER
-        socket.to(Room).emit('RoomNotice',`${userName} joined the group`);
+    // ADMIN CONNECT
+    socket.on('adminJoin', () => {
+        adminSocketId = socket.id;
+        console.log('Admin connected with socket id:',adminSocketId);
     });
 
-    // CHAT MESSAGE
-    socket.on('chatMessage', (msg) => {
-        console.log('Message:', msg);
-        // SEND MESSAGE TO OTHERS
-        socket.to(Room).emit('chatMessage', msg);
+    // USER MESSAGE TO ADMIN
+    socket.on('sendToAdmin', (msgData) => {
+        console.log('User Message:', msgData);
+        // SEND ONLY TO ADMIN
+        io.to(adminSocketId).emit(
+            'receiveUserMessage',
+            msgData
+        );
+    });
+
+    // ADMIN REPLY TO USER
+    socket.on('adminReply', (data) => {
+        console.log('Admin Reply:', data);
+        io.to(data.targetSocketId).emit(
+            'receiveAdminReply',
+            data
+        );
     });
 
     // DISCONNECT
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
+        console.log('Disconnected:', socket.id);
+        // REMOVE ADMIN SOCKET
+        if (socket.id === adminSocketId) {
+            adminSocketId = null;
+        }
     });
 
 });
@@ -58,7 +74,7 @@ io.on('connection', (socket) => {
 const startServer = async () => {
     await ConnectMongoose();
     server.listen(process.env.PORT || 5000, () => {
-        console.log(` Server running on port ${process.env.PORT || 5000}`);
+        console.log(`🚀 Server running on port ${process.env.PORT || 5000}`);
     });
 };
 
