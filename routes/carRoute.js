26c989c66f9e2338c9574605b039e9a29cee16c5
Prem1kr <prem78334@gmail.com>
2026-05-09 15:44:52 +0530

    get car added

--- routes/carRoute.js
+++ routes/carRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {AddCar,EditCar,RemoveCar} from '../controllers/carController.js';
+import { AddCar, EditCar, getCar, RemoveCar } from '../controllers/carController';
 
 const carRouter = express.Router();
 
-carRouter.post('/add-car', Addcar);
+carRouter.post('/add-car', AddCar);
 carRouter.put('/edit-car/:id', EditCar);
 carRouter.delete('/delete-car/:id', RemoveCar);
+carRouter.get('/get-car', getCar);
 
 export default carRouter;
