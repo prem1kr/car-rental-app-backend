b56a6a7cf78ba0f1d3c0c907b6dfd3d1912e8674
Prem1kr <prem78334@gmail.com>
2026-05-12 13:00:49 +0530

    correction in profile controller

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -6,8 +6,8 @@ export const ProfielEdit = async (req, res) => {
 
         const profile = await profileModel.findOne({ email, name });
         if (!profile) {
-            return res.status(404).json({ message: 'User not found, Login first' }, profile);
-            console.log(`user not found, Login first`);
+            console.log('user not found');
+            return res.status(404).json({ success: false, message: 'User not found, Login first' });
         }
         const updatedProfile = await profileModel.findOneAndUpdate({ email, name }, {
             phone,
@@ -32,8 +32,7 @@ export const GetProfile = async (req, res) => {
         if (!profile) {
             return res.status(404).json({ success: false, message: 'Profile not found' });
         }
-        return res.status(200).json({ success: true, message: 'profile fetched successfully' });
-
+        return res.status(200).json({ success: true, message: 'profile fetched successfully', profile });
     } catch (error) {
         return res.status(500).json({ success: false, message: 'Server Error', error: error.message });
         console.log(error);
