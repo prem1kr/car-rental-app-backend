a75260d61ad33a11630e9027f1f7cb20f75e957b
Prem1kr <prem78334@gmail.com>
2026-05-13 19:07:51 +0530

    p

--- config/mail.js
+++ config/mail.js
@@ -1,13 +1,18 @@
 import nodemailer from 'nodemailer';
-import dns from 'dns';
-
-dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+    },
+
+    tls: {
+        rejectUnauthorized: false,
+        family: 4, // force IPv4
     },
 });
 
