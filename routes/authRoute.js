85a4577f0a122a9602eb3e87083808f5d68a3d26
prem kumar <151729765+prem1kr@users.noreply.github.com>
2026-05-25 13:10:08 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { Login, Logout, Signup, TotalUser, User } from "../controllers/authController.js";
-// import { authMiddleware } from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', Signup);
 authRouter.post('/login', Login);
-authRouter.get('/user', User);
+authRouter.get('/user',authMiddleware, User);
 authRouter.post('/logout', Logout);
 authRouter.get('/total-user', TotalUser);
 
