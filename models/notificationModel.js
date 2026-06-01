13a7180c59b2ab6be1a92eebec0ad1d27ee92684
prem kumar <151729765+prem1kr@users.noreply.github.com>
2026-06-01 11:10:47 +0530

    Update notificationModel.js

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -1,31 +1,10 @@
 import mongoose from "mongoose";
 
 const notificationSchema = new mongoose.Schema({
-    import mongoose from "mongoose";
-
-const notificationSchema = new mongoose.Schema({
      userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     },
-    title:{
-        type: String,
-        required: true,
-        trim: true
-    },
-    message:{
-        type: String,
-        require: true,
-        trim: true
-    },
-    read:{
-        type: Boolean,
-        default: false
-    }
-
-},{timestamps:true});
-
-export default mongoose.model('notification', notificationSchema);
     title:{
         type: String,
         required: true,
